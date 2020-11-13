f969531949931254d72ead7fabfb38335671773e
hans-vvv <33095823+hans-vvv@users.noreply.github.com>
2020-11-13 07:17:19 +0100

    More refactoring
    More refactoring

--- ios_xe_parser.py
+++ ios_xe_parser.py
@@ -1,4 +1,75 @@
-from utils import ReSearcher, Tree, LineParser, splitrange, get_key
+import re
+import json
+from utils import ReSearcher, Tree, splitrange, get_key, get_value
+
+
+class InterfaceParser():
+
+    """
+    Helper class to parse interface items. Items which are
+    present in list_items are stored in lists.
+    With using the _select_key method the following logic is implemented
+    to determine which part of an interface item is considered to be a
+    key and which part a value.
+
+    1. First portkey_exceptions list is examined. If an interface item
+       contains the words found in this list then key = item in the list
+       and value = remaining words of the interface item. If an interface
+       item is found then the other methods are not considered.
+    2. Key_length dict is examined. If interface item contains an item
+       found in a list of the dict then corresponding key (i.e. 1 or 2)
+       is used to split the item. The key of the item is equal to the
+       number of words of the dict key, the rest of the item = value.
+       Example: If line = channel-group 2 mode active, then
+       key = "channel-group"  and value = "2 mode active". If an interface
+       item is found then the last method is not considered.
+    3. Default method. Last word of line = value
+       and all preceding words = key.
+    """
+
+    def __init__(self, list_items, key_exceptions, key_length):
+        self.list_items = list_items
+        self.key_exceptions = key_exceptions
+        self.key_length = key_length
+
+    def initialize_lists(self):
+        self.values = [[] for item in self.list_items]
+
+    def _get_index(self, line):
+        """ Get index by name of lists to store values """
+        for index, item in enumerate(self.list_items):
+            if line.startswith(item):
+                return index
+
+    def _select_key(self, line):
+        """ Determine key-value split of items based on algorithm """
+        for key in self.key_exceptions:
+            if line.startswith(key):
+                return key
+        for key_length, items in self.key_length.items():
+            for item in items:
+                if line.startswith(item):
+                    return get_key(line, key_length)
+        return get_key(line, len(line.split())-1)
+            
+    def parse_line(self, tree, portindex, line):
+        """ Parse line into dict where value is str, list or extended list """
+        line = line.lstrip()
+        key = self._select_key(line)
+        for item in self.list_items:
+            if line.startswith(item):
+                index = self._get_index(line)
+                if line.startswith('switchport trunk allowed vlan'):
+                    self.values[index].extend(splitrange(get_value(key, line)))
+                    tree['port'][portindex]['vlan_allow_list'] \
+                                                         = self.values[index]
+                    return tree
+                else:
+                    self.values[index].append(get_value(key, line))
+                    tree['port'][portindex][item] = self.values[index]
+                    return tree
+        tree['port'][portindex][key] = get_value(key, line)
+        return tree
 
 
 def ios_xe_parser(configfile):
@@ -8,7 +79,10 @@ def ios_xe_parser(configfile):
     dictionary. It has the following capabilities:
     - All interfaces items are parsed into key-value pairs using an
       algorithm to determine the key and value parts of the interface items.
-    - For specific items, lists are used to store data.
+    - For specific interface items, lists are used to store data.
+    - Global config items are stored in a list.
+    - All hierarchical items are stored in seperate lists.
+    - Banners are stored in a list.
     """
 
     with open(configfile, 'r') as lines:
@@ -24,63 +98,42 @@ def ios_xe_parser(configfile):
         }
         list_items = ['switchport trunk allowed vlan', 'standby',
                       'ip helper-address', 'logging event']
-        line_parser = LineParser(list_items, key_exceptions, key_length)
+        port_parser = InterfaceParser(list_items, key_exceptions, key_length)
 
         context = ''
-        vlanindex = ''
-      
+              
         for line in lines:
             
             if not line.strip(): # skip empty lines
                 continue
+            
             line = line.rstrip()
                         
             if match(r'^hostname (.*)', line):
                 hostname = format(match.group(1))
                 tree['hostname'] = hostname
                      
-            elif match(r'^interface (Vlan(\d+))', line):
-                context = 'port'
-                portindex = format(match.group(1))
-                vlanindex = format(match.group(2))
-                tree['vlan'][vlanindex] = {}
-                line_parser.initialize_lists()
-  
             elif match(r'^interface (.*)', line):
                 context = 'port'
                 portindex = format(match.group(1))
                 tree['port'][portindex] = {}
-                line_parser.initialize_lists()
+                port_parser.initialize_lists()
  
-            elif match(r'^vlan (\d+)$', line):
+            elif match(r'^vlan ([\d,-]+)', line):
                 context = 'vlan'
-                vlanindex = format(match.group(1))
-                tree['vlan'][vlanindex] = {}
-
-            elif match(r'^vlan ([0-9,-]+)', line):
-                context = 'vlan'
-                value = format(match.group(1))
-                for raw_vlans in value.split(','):
-                    if '-' in raw_vlans:
-                        for vlan in splitrange(raw_vlans):
-                            tree['vlan'][str(vlan)] = {}
-                    else:
-                        tree['vlan'][raw_vlans] = {}
-
+                for vlan in splitrange(format(match.group(1))):
+                    tree['vlan'][vlan] = {}
+                
             elif context == 'port':
 
                 if match(r'^ no (.*)', line):
                     key = format(match.group(1))
                     value = format(match.group(0))
-                    if 'Vlan' in portindex:
-                        tree['vlan'][vlanindex][key] = value
-                    else:
-                        tree['port'][portindex][key] = value
+                    tree['port'][portindex][key] = value
 
                 # interface items are stored with helper class
                 elif match('^ .*', line):
-                    args = tree, portindex, vlanindex, line
-                    tree = line_parser.parse_line(*args)
+                    tree = port_parser.parse_line(tree, portindex, line)
 
                 elif match(r'!$', line):
                     context = ''
@@ -88,9 +141,9 @@ def ios_xe_parser(configfile):
             elif context == 'vlan':
 
                 if match(r'^ name (.*)', line):
-                    tree['vlan'][vlanindex]['name'] = format(match.group(1))
+                    tree['vlan'][vlan]['name'] = format(match.group(1))
 
                 elif match(r'!$', line):
                     context = ''
-
+       
         return tree
--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import os
 import json
 from openpyxl.utils import get_column_letter
 
-
 class ReSearcher():
     
     """
@@ -30,87 +29,6 @@ class Tree(dict):
     def __str__(self):
         """ Serialize dictionary to JSON formatted string with indents """
         return json.dumps(self, indent=4)
-
-
-class LineParser():
-
-    """
-    Helper class to parse interface items. Items which are
-    present in list_items are stored in lists.
-    With using the _select_key method the following logic is implemented
-    to determine which part of an interface item is considered to be a
-    key and which part a value.
-
-    1. First portkey_exceptions list is examined. If an interface item
-       contains the words found in this list then key = item in the list
-       and value = remaining words of the interface item. If an interface
-       item is found then the other methods are not considered.
-    2. Key_length dict is examined. If interface item contains an item
-       found in a list of the dict then corresponding key (i.e. 1 or 2)
-       is used to split the item. The key of the item is equal to the
-       number of words of the dict key, the rest of the item = value.
-       Example: If line = channel-group 2 mode active, then
-       key = "channel-group"  and value = "2 mode active". If an interface
-       item is found then the last method is not considered.
-    3. Default method. Last word of line = value
-       and all preceding words = key.
-    """
-
-    def __init__(self, list_items, key_exceptions, key_length):
-        self.list_items = list_items
-        self.key_exceptions = key_exceptions
-        self.key_length = key_length
-
-    def initialize_lists(self):
-        self.values = [[] for item in self.list_items]
-
-    def _get_index(self, line):
-        for index, item in enumerate(self.list_items):
-            if line.startswith(item):
-                return index
-
-    def _select_key(self, line):
-        for key in self.key_exceptions:
-            if line.startswith(key):
-                return key
-        for key_length, items in self.key_length.items():
-            for item in items:
-                if line.startswith(item):
-                    return get_key(line, key_length)
-        return get_key(line, len(line.split())-1)
-            
-
-    def parse_line(self, tree, portindex, vlanindex, line):
-
-        line = line.lstrip()
-        key = self._select_key(line)
-
-        if line.startswith('switchport trunk allowed vlan'):
-            index = self._get_index(line)
-            for raw_vlans in get_value(key, line).split(','):
-                if '-' in raw_vlans:
-                    for vlan_id in splitrange(raw_vlans):
-                        self.values[index].append(vlan_id)
-                else:
-                    self.values[index].append(raw_vlans)
-            tree['port'][portindex]['vlan_allow_list'] = self.values[index]
-            return tree
-
-        for item in self.list_items:
-            if line.startswith(item):
-                index = self._get_index(line)
-                self.values[index].append(get_value(key, line))
-                if 'Vlan' in portindex:
-                    tree['vlan'][vlanindex][item] = self.values[index]
-                else:
-                    tree['port'][portindex][item] = self.values[index]
-                return tree
-
-        if 'Vlan' in portindex:
-            tree['vlan'][vlanindex][key] = get_value(key, line)
-        else:
-            tree['port'][portindex][key] = get_value(key, line)
-        return tree
 
 
 def get_value(key, item):
@@ -149,7 +67,8 @@ def get_key(item, key_length):
 def splitrange(raw_range):
 
     """
-    ex. splitrange('105-107') will return ['105','106','107']
+    '1,2,4-6' returns ['1','2','4','5','6']
+    'none'    returns ['None']
     """
 
     m = re.search(r'^(\d+)\-(\d+)$', raw_range)
@@ -157,6 +76,21 @@ def splitrange(raw_range):
         first = int(format(m.group(1)))
         last = int(format(m.group(2)))
         return [str(i) for i in range(first, last+1)]
+
+    m = re.search(r'[\d+,-]+', raw_range)
+    if m:
+        result = []
+        for raw_element in format(m.group(0)).split(','):
+            if '-' in raw_element:
+                for element in splitrange(raw_element):
+                    result.append(element)
+            else:
+                result.append(raw_element)
+        return result
+
+    m = re.search(r'^none$', raw_range)
+    if m:        
+        return ['None']
 
 
 def xlref(row, column, zero_indexed=True):
@@ -168,6 +102,3 @@ def xlref(row, column, zero_indexed=True):
         row += 1
         column += 1
     return get_column_letter(column) + str(row)
-
-
-
